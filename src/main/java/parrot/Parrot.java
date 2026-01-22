857fa1e8c52cf435e723a8e9e016f12920230d45
vinod <vinod@wealthos.cloud>
2026-01-22 22:07:39 +0530

    move createParrot to ParrotTypeEnum

--- src/main/java/parrot/Parrot.java
+++ src/main/java/parrot/Parrot.java
@@ -2,17 +2,6 @@ package parrot;
 
 public abstract class Parrot {
 
-    public Parrot() {
-    }
-
-    public static Parrot createParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
-        return switch (type) {
-            case EUROPEAN -> new EuropeanParrot();
-            case AFRICAN -> new AfricanParrot(numberOfCoconuts);
-            case NORWEGIAN_BLUE -> new NorwegianBlueParrot(voltage, isNailed);
-        };
-    }
-
     public abstract double getSpeed();
     public abstract String getCry();
 
--- src/main/java/parrot/ParrotTypeEnum.java
+++ src/main/java/parrot/ParrotTypeEnum.java
@@ -4,5 +4,13 @@ public enum ParrotTypeEnum {
 
     EUROPEAN,
     AFRICAN,
-    NORWEGIAN_BLUE
+    NORWEGIAN_BLUE;
+
+    public static Parrot createParrot(ParrotTypeEnum type, int numberOfCoconuts, double voltage, boolean isNailed) {
+        return switch (type) {
+            case EUROPEAN -> new EuropeanParrot();
+            case AFRICAN -> new AfricanParrot(numberOfCoconuts);
+            case NORWEGIAN_BLUE -> new NorwegianBlueParrot(voltage, isNailed);
+        };
+    }
 }
